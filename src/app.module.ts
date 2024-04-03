bd5d54f6d85e86e8cb0356938a5e71361e57713f
Samuel Leiva Lier <saleiva@bancolombia.com.co>
2024-04-03 12:16:05 -0500

    Prueba de middleware sencillo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -21,4 +23,10 @@ import { PostsModule } from './posts/posts.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(UsersController)
+  }
+}
