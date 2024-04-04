cfc2f1bc7a4ebb199eb572702637dd8ddeedf939
Samuel Leiva Lier <saleiva@bancolombia.com.co>
2024-04-04 10:53:26 -0500

    Middleware y validaciones agregados

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from "express";
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
-        console.log('Se consulta una ruta de users...');
+        console.log('Se está ejecutando logger...');
         next();
       }
 }
--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,24 @@
+import { IsInt, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator"
+
 export class CreatePostDto{
+    @IsString()
+    @MinLength(3, {
+        message: "Title is too short."
+    })
+    @MaxLength(100, {
+        message: "Title is too long."
+    })
+    @IsNotEmpty({
+        message: "Title is required."
+    })
     title: string
+
+    @IsString()
+    @MaxLength(400, {
+        message: "Content is too long."
+    })
     content: string
+
+    @IsInt()
     authorId: number
 }
